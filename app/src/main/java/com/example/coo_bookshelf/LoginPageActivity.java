2306f008b0d15a50dcaa860d4dcd9c03a65a67bc
Armando Sarza <asarza.pro97@gmail.com>
2025-12-06 00:00:44 -0800

    Login Page Commit: (Unfinished) Added Log in stub. Created Intents from Login to Main.

--- app/src/main/java/com/example/coo_bookshelf/LoginPageActivity.java
+++ app/src/main/java/com/example/coo_bookshelf/LoginPageActivity.java
@@ -1,24 +1,43 @@
 package com.example.coo_bookshelf;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.coo_bookshelf.databinding.ActivityLoginPageBinding;
 
 public class LoginPageActivity extends AppCompatActivity {
+
+    // Through binding, we can access all views in activity_login_page.xml.
+    private ActivityLoginPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Inflate creates the screen from the XML layout.
+        binding = ActivityLoginPageBinding.inflate(getLayoutInflater());
+        // Shows the layout on the screen
+        setContentView(binding.getRoot());
+
+        // Action for login button when clicked.
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make a plan to go to MainActivity after login.
+                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(),
+                        0);
+                // TODO: Replace with actual logged in user ID
+                startActivity(intent);
+            }
         });
     }
+
+    /** Lets us jump to this activity from other activities */
+    static Intent loginIntentFactory(Context context) {
+        return new Intent(context, LoginPageActivity.class);
+    }
+
 }
