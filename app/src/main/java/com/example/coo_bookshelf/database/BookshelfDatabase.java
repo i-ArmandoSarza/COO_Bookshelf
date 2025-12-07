40f6e6f537e9d4e63a1cb4a153433b34640ea2a2
Steve Mendoza <smendoza88@gmail.com>
2025-12-07 15:03:33 -0800

    - Updated BookshelfDatabase to reseed data when the version number is updated.

--- app/src/main/java/com/example/coo_bookshelf/database/BookshelfDatabase.java
+++ app/src/main/java/com/example/coo_bookshelf/database/BookshelfDatabase.java
@@ -8,6 +8,7 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 import com.example.coo_bookshelf.MainActivity;
+import com.example.coo_bookshelf.database.DAO.BookDAO;
 import com.example.coo_bookshelf.database.entities.Book;
 import com.example.coo_bookshelf.database.entities.BookCategory;
 import com.example.coo_bookshelf.database.entities.Category;
@@ -17,7 +18,7 @@ import java.util.concurrent.Executors;
 
 
 @Database(entities = {User.class, Book.class, Category.class,
-    BookCategory.class}, version = 14, exportSchema = false)
+    BookCategory.class}, version = 8, exportSchema = false)
 public abstract class BookshelfDatabase extends RoomDatabase {
 
   private static final int NUMBER_OF_THREADS = 4;
@@ -31,34 +32,84 @@ public abstract class BookshelfDatabase extends RoomDatabase {
     @Override
     public void onCreate(@NonNull SupportSQLiteDatabase db) {
       super.onCreate(db);
-      // Set up default accounts
-      Log.i(MainActivity.TAG, "DATABASE Creating users!"); // TODO: Remove this. This is for testing purposes.
-      databaseWriteExecutor.execute(() -> {
-        UserDAO userDAO = INSTANCE.userDAO();
-        userDAO.deleteAll();
-        User admin = new User("monty@csumb.edu", "admin1");
-        admin.setAdmin(true);
-        admin.setFirstName("Monty Rey");
-        userDAO.insert(admin);
-        Log.i(MainActivity.TAG, "Creating user Monty!");
+      // This only runs the first time and the database is created.
+      Log.i(MainActivity.TAG,
+          "DATABASE Creating users!"); // TODO: Remove this. This is for testing purposes.
 
-//        User testUser1 = new User("testuser1", "testuser1");
-//        testUser1.setFirstName("Katrina");
-//        userDAO.insert(testUser1);
-        Log.i(MainActivity.TAG, "Creating user Katrina and books record.");
-        db.execSQL(
-        """
-          insert into User (Email, password, firstName, isAdmin)
-          values ('katcsumb', 'katcsumb', 'Katrina', 0);
-        """.stripIndent());
-        db.execSQL(
-      """
-          insert into Book(UserId, Title, Author, AuthorKey, FirstPublishedYear, WorksID)
-          values (last_insert_rowid(), 'Red Rising', 'Pierce Brown', 'OL7621609A', 'October 14, 2025', 'OL17076473W');
-         """.stripIndent());
+      Log.i(MainActivity.TAG, "Creating user Monty!");
+
+      db.execSQL(
+          """
+                insert into User (Email, password, firstName, isAdmin)
+                values ('monty@csumb.edu', 'admin1', 'Monty Rey', 1);
+              """.stripIndent()
+      );
+      db.execSQL(
+          """
+                insert into Book(UserId, Title, Author, AuthorKey, FirstPublishedYear, WorksID)
+                values (last_insert_rowId(), 'Red Rising', 'Pierce Brown', 'OL7621609A', 'October 14, 2025', 'OL17076473W');
+              """.stripIndent()
+      );
+
+      // Create user Katrina and books record with raw sql.
+      Log.i(MainActivity.TAG, "Creating user Katrina and books record.");
+      db.execSQL(
+          """
+                insert into User (Email, password, firstName, isAdmin)
+                values ('katcsumb', 'katcsumb', 'Katrina', 0);
+              """.stripIndent()
+      );
+      db.execSQL(
+          """
+                insert into Book(UserId, Title, Author, AuthorKey, FirstPublishedYear, WorksID)
+                values (last_insert_rowId(), 'Red Rising', 'Pierce Brown', 'OL7621609A', 'October 14, 2025', 'OL17076473W');
+              """.stripIndent()
+      );
+    }
+
+    // onDestructiveMigration only runs when the database version is upgraded, and will reseed data.
+    @Override
+    public void onDestructiveMigration(@NonNull SupportSQLiteDatabase db) {
+      super.onDestructiveMigration(db);
+      databaseWriteExecutor.execute(() -> {
+        Log.i(MainActivity.TAG, "onDestructiveMigration: Re-seeding database.");
+        // DAO setup
+        UserDAO userDAO = INSTANCE.userDAO();
+        BookDAO bookDAO = INSTANCE.bookDAO();
+
+        Log.i(MainActivity.TAG, "onDestructiveMigration: Creating Monty's User record.");
+        // Creating Monty seed created.
+        User userMonty = new User("monty@csumb.edu", "admin1");
+        userMonty.setAdmin(true);
+        userMonty.setFirstName("Monty Rey");
+        userDAO.insert(userMonty);
+
+        Log.i(MainActivity.TAG, "onDestructiveMigration: Creating Monty's Book record.");
+        // add monty's book info
+        var montyUserId = userDAO.getUserByUserEmailSync("monty@csumb.edu");
+        Book montyBook = new Book(montyUserId, "Red Rising",
+            "Pierce Brown", "OL7621609A", "OL17076473W");
+        montyBook.setFirstPublishedYear("October 14, 2025");
+        bookDAO.insert(montyBook);
+
+        Log.i(MainActivity.TAG, "onDestructiveMigration: Creating Katrina's User record.");
+        // Create seed record for Katrina non admin user
+        User kat = new User("kat@csumb.edu", "password1");
+        kat.setFirstName("Katrina");
+        kat.setAdmin(false);
+        userDAO.insert(kat);
+
+        Log.i(MainActivity.TAG, "onDestructiveMigration: Creating Katrina's Book record.");
+        // Creating seed record for Katrina's book.
+        var katUserId = userDAO.getUserByUserEmailSync("kat@csumb.edu");
+        Book katBook = new Book(montyUserId, "The Kite Runner",
+            "Khaled Hossein", "OL1412764A", "OL15902631W");
+        katBook.setFirstPublishedYear("2004");
+        bookDAO.insert(katBook);
       });
     }
   };
+
   static BookshelfDatabase getDatabase(final Context context) {
     Log.i(MainActivity.TAG, "getDatabase");
     if (INSTANCE == null) {
@@ -72,7 +123,8 @@ public abstract class BookshelfDatabase extends RoomDatabase {
               .fallbackToDestructiveMigration(true)
               .addCallback(addDefaultValues)
               .build();
-          Log.i(MainActivity.TAG, "getDatabase synchronized"); // TODO: Remove this. This is for testing purposes.
+          Log.i(MainActivity.TAG,
+              "getDatabase synchronized"); // TODO: Remove this. This is for testing purposes.
         }
       }
     }
@@ -80,7 +132,9 @@ public abstract class BookshelfDatabase extends RoomDatabase {
     return INSTANCE;
   }
 
-  //TODO: Add DOA here.
+  //TODO: Add DOA's here.
   public abstract UserDAO userDAO();
+
+  public abstract BookDAO bookDAO();
 
 }
