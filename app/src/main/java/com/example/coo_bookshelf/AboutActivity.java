c8be9c84c19c9bc261831ea1ed3bbe40da3498af
kristine-barr <kristinebarruzo@gmail.com>
2025-12-12 14:43:15 -0800

    Add About page (Activity + XML layout) with back arrow support.

---
+++ app/src/main/java/com/example/coo_bookshelf/AboutActivity.java
@@ -0,0 +1,40 @@
+package com.example.coo_bookshelf;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coo_bookshelf.databinding.ActivityAboutBinding;
+
+public class AboutActivity extends AppCompatActivity {
+
+    ActivityAboutBinding binding;
+
+    public static Intent aboutIntentFactory(Context context) {
+        return new Intent(context, AboutActivity.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(binding.getRoot());
+
+        // Setup Toolbar: back button
+        setSupportActionBar(binding.toolbar);
+
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);    // hide title text
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);      // show back arrow
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();   // go back to previous Activity
+        return true;
+    }
+}
